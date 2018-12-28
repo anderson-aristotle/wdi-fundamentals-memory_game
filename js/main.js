c5bb3605a63fb40339f38586a55464259c7505b9
Dale Anderson <aristotle@Aristotles-MacBook-Pro.local>
2018-12-28 11:01:36 -0500

    created logic game

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen match";
+var cardFour = "king match";
+
+console.log("user flipped" + cardOne);
+console.log("user flipped" + cardTwo);
+
+
