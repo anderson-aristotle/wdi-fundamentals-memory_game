53389fcc48569295c4046b0f25f11b522a3bc835
Dale Anderson <aristotle@Aristotles-MacBook-Pro.local>
2018-12-29 11:55:49 -0500

    all files of memory_game

--- js/main.js
+++ js/main.js
@@ -6,13 +6,31 @@ console.log("player flipped king.");
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTw0);
+cardsInPlay.push(cardTwo);
+
+//create flipCard function
+var flipCard = function () {
+
+	var cardID = this.getAttribute('data-id');
+	console.log("User flipped" + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
+	checkForMatch();
+}
+var cardID();
+console.log("User flipped" + cards[cardID]);
 
 //create variable for the first card flipped
-cardOne[0];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log ("User flipped" + cardOne);
 
 //create variable to rep the second card flipped
-cardTwo[2];
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + cardTwo);
 
 //if statement that checks to see if the length of the cardsInPlay array is 2
 if (cardsInPlay [0] === cardsInPlay [2] ) {
@@ -21,4 +39,4 @@ if (cardsInPlay [0] === cardsInPlay [2] ) {
 	alert ('Try again.');
 }
 
-//////
+
